57e1edaacf1b5ee588732b61db4479b67a424b30
bullpae <bullpae@gmail.com>
2024-08-27 15:44:00 +0900

    bastion, dev 서버 추가

--- kips_cloud/kips_main.tf
+++ kips_cloud/kips_main.tf
@@ -1,6 +1,42 @@
 # kips_main.tf
 
 # Network Interface 생성
+resource "nhncloud_networking_port_v2" "bastion-nic" {
+  name = "web_port_1"
+  network_id = nhncloud_networking_vpc_v2.kips-vpc.id
+  admin_state_up = "true"
+  fixed_ip {
+    subnet_id = nhncloud_networking_vpcsubnet_v2.web-subnet.id
+  }
+  security_group_ids = [nhncloud_networking_secgroup_v2.web-sg.id]
+}
+
+# resource "nhncloud_networking_floatingip_associate_v2" "bastion-fip_associate" {
+#     floating_ip = nhncloud_networking_floatingip_v2.bastion-fip.address
+#     port_id = nhncloud_networking_port_v2.bastion-nic.id
+# }
+
+# web 서버 인스턴스 생성
+resource "nhncloud_compute_instance_v2" "bastion-instance" {
+    name      = "bastion-server"
+    key_pair  = "kips_key"
+    flavor_id = data.nhncloud_compute_flavor_v2.t2c1m1.id
+
+    network {        
+        port = nhncloud_networking_port_v2.bastion-nic.id        
+    }    
+
+    block_device {
+        uuid                  = data.nhncloud_images_image_v2.linux.id
+        source_type           = "image"
+        destination_type      = "volume"
+        boot_index            = 0
+        volume_size           = 20
+        delete_on_termination = true
+    }
+}
+
+# Network Interface 생성
 resource "nhncloud_networking_port_v2" "web-nic" {
   name = "web_port_1"
   network_id = nhncloud_networking_vpc_v2.kips-vpc.id
@@ -55,6 +91,37 @@ resource "nhncloud_compute_instance_v2" "was-instance" {
 
     network {        
         port = nhncloud_networking_port_v2.was-nic.id        
+    }    
+
+    block_device {
+        uuid                  = data.nhncloud_images_image_v2.linux.id
+        source_type           = "image"
+        destination_type      = "volume"
+        boot_index            = 0
+        volume_size           = 20
+        delete_on_termination = true
+    }
+}
+
+# Network Interface 생성
+resource "nhncloud_networking_port_v2" "dev-nic" {
+  name = "was_port_1"
+  network_id = nhncloud_networking_vpc_v2.kips-vpc.id
+  admin_state_up = "true"
+  fixed_ip {
+    subnet_id = nhncloud_networking_vpcsubnet_v2.dev-subnet.id
+  }
+  security_group_ids = [nhncloud_networking_secgroup_v2.dev-sg.id]
+}
+
+# dev 서버 인스턴스 생성
+resource "nhncloud_compute_instance_v2" "dev-instance" {
+    name      = "dev-server"
+    key_pair  = "kips_key"
+    flavor_id = data.nhncloud_compute_flavor_v2.t2c1m1.id
+
+    network {        
+        port = nhncloud_networking_port_v2.dev-nic.id        
     }    
 
     block_device {
--- kips_cloud/security_group.tf
+++ kips_cloud/security_group.tf
@@ -1,6 +1,22 @@
 # security_group.tf 
 
 # 보안 그룹 생성
+# Bastion
+resource "nhncloud_networking_secgroup_v2" "bastion-sg" {
+    name = "bastion-sg"
+}
+
+resource "nhncloud_networking_secgroup_rule_v2" "bastion-sg-rule-01" {
+    direction         = "ingress"
+    ethertype         = "IPv4"
+    protocol          = "tcp"
+    port_range_min    = 22
+    port_range_max    = 22
+    remote_ip_prefix  = "0.0.0.0/0"
+    security_group_id = nhncloud_networking_secgroup_v2.web-sg.id
+}
+
+# WEB
 resource "nhncloud_networking_secgroup_v2" "web-sg" {
     name = "web-sg"
 }
@@ -15,16 +31,7 @@ resource "nhncloud_networking_secgroup_rule_v2" "web-sg-rule-01" {
     security_group_id = nhncloud_networking_secgroup_v2.web-sg.id
 }
 
-resource "nhncloud_networking_secgroup_rule_v2" "web-sg-rule-02" {
-    direction         = "ingress"
-    ethertype         = "IPv4"
-    protocol          = "tcp"
-    port_range_min    = 22
-    port_range_max    = 22
-    remote_ip_prefix  = "0.0.0.0/0"
-    security_group_id = nhncloud_networking_secgroup_v2.web-sg.id
-}
-
+# WAS
 resource "nhncloud_networking_secgroup_v2" "was-sg" {
     name = "was-sg"
 }
@@ -35,7 +42,7 @@ resource "nhncloud_networking_secgroup_rule_v2" "was-sg-rule-01" {
     protocol          = "tcp"
     port_range_min    = 8080
     port_range_max    = 8080
-    remote_ip_prefix  = "10.0.0.0/8"
+    remote_ip_prefix  = "10.0.0.0/16"
     security_group_id = nhncloud_networking_secgroup_v2.was-sg.id
 }
 
@@ -45,10 +52,11 @@ resource "nhncloud_networking_secgroup_rule_v2" "was-sg-rule-02" {
     protocol          = "tcp"
     port_range_min    = 22
     port_range_max    = 22
-    remote_ip_prefix  = "10.0.0.0/8"
+    remote_ip_prefix  = "10.0.0.0/16"
     security_group_id = nhncloud_networking_secgroup_v2.was-sg.id
 }
 
+# DB
 resource "nhncloud_networking_secgroup_v2" "db-sg" {
     name = "db-sg"
 }
@@ -59,7 +67,7 @@ resource "nhncloud_networking_secgroup_rule_v2" "db-sg-rule-01" {
     protocol          = "tcp"
     port_range_min    = 3306
     port_range_max    = 3306
-    remote_ip_prefix  = "10.0.0.0/8"
+    remote_ip_prefix  = "10.0.0.0/16"
     security_group_id = nhncloud_networking_secgroup_v2.db-sg.id
 }
 
@@ -69,6 +77,21 @@ resource "nhncloud_networking_secgroup_rule_v2" "db-sg-rule-02" {
     protocol          = "tcp"
     port_range_min    = 22
     port_range_max    = 22
-    remote_ip_prefix  = "10.0.0.0/8"
+    remote_ip_prefix  = "10.0.0.0/16"
     security_group_id = nhncloud_networking_secgroup_v2.db-sg.id
+}
+
+# DEV
+resource "nhncloud_networking_secgroup_v2" "dev-sg" {
+    name = "dev-sg"
+}
+
+resource "nhncloud_networking_secgroup_rule_v2" "dev-sg-rule-01" {
+    direction         = "ingress"
+    ethertype         = "IPv4"
+    protocol          = "tcp"
+    port_range_min    = 22
+    port_range_max    = 22
+    remote_ip_prefix  = "10.0.0.0/16"
+    security_group_id = nhncloud_networking_secgroup_v2.dev-sg.id
 }
