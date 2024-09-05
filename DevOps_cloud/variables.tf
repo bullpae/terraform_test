9c080ac8f8e66da12d31a18fd3070d281e178b50
bullpae <bullpae@gmail.com>
2024-09-05 17:55:51 +0900

    Provider로 옮김

--- DevOps_cloud/variables.tf
+++ DevOps_cloud/variables.tf
@@ -1,26 +1,3 @@
-variable "nhn_user_name" {
-  description = "nhncloud account"
-  type        = string
-}
-
-# Compute - Instance - API 엔드포인트 - API 엔드포인트 설정 - 테넌트 ID
-variable "api_tenant_id" {
-  description = "Tenant ID"
-  type        = string
-}
-
-# Compute - Instance - API 엔드포인트 - API 엔드포인트 설정 - API 비밀번호 설정
-variable "api_password" {
-  description = "API Password"
-  type        = string
-}
-
-variable "api_identity" {
-  description = "api identity url:"
-  type        = string
-  default     = "https://api-identity-infrastructure.nhncloudservice.com/v2.0"
-}
-
 variable "subnet_zones" {
   type    = list(string)
   default = ["devpos", "test"]
