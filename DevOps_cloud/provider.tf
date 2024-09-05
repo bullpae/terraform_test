a87723158b974d3aa6b447644a24ccf6b7331ec6
bullpae <bullpae@gmail.com>
2024-09-05 17:54:58 +0900

    Provider로 옮김

--- DevOps_cloud/provider.tf
+++ DevOps_cloud/provider.tf
@@ -14,6 +14,23 @@ provider "nhncloud" {
   user_name = var.nhn_user_name
   tenant_id = var.api_tenant_id
   password  = var.api_password
-  auth_url  = var.api_identity
+  auth_url  = "https://api-identity-infrastructure.nhncloudservice.com/v2.0"
   region    = "KR1"
+}
+
+variable "nhn_user_name" {
+  description = "nhncloud account"
+  type        = string
+}
+
+# Compute - Instance - API 엔드포인트 - API 엔드포인트 설정 - 테넌트 ID
+variable "api_tenant_id" {
+  description = "Tenant ID"
+  type        = string
+}
+
+# Compute - Instance - API 엔드포인트 - API 엔드포인트 설정 - API 비밀번호 설정
+variable "api_password" {
+  description = "API Password"
+  type        = string
 }
